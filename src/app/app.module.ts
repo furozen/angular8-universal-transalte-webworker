0070ce7fe2ec1880c8791becf338ac06aba7192d
andrewm <andrewm@smartinfosys.com>
2019-06-19 19:55:32 +0300

    init layout and init menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ToolbarMenuComponent } from './toolbar-menu/toolbar-menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarMenuComponent
   ],
   imports: [
     BrowserModule
